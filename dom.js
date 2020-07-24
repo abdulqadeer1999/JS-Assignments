cc4a65a45c601c8b8f4f25f22ac2d2a55bbda5f3
abdulqadeer1999 <57321409+abdulqadeer1999@users.noreply.github.com>
2020-07-24 10:39:59 +0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,67 @@
+
+// Chap 58-67
+// Task 1
+
+
+//i.
+
+// var a = document.getElementById("main-content");
+// console.log(a.nodeName)
+
+//ii.
+
+// var a = document.getElementById("main-content");
+// console.log(a.childNodes)
+
+//iii.
+
+// var d = document.getElementsByClassName("render"); 
+//  var p = d.childNodes; 
+// console.log(p.innerHTML);
+
+//iv.
+// var a = document.getElementById("first-name");
+// var newTxt = document.createTextNode("Mahnoor");
+// a.appendChild(newTxt);
+
+//v.
+
+// var a = document.getElementById("last-name")
+// var newTxt = document.createTextNode("Shoukat");
+// a.appendChild(newTxt);
+
+// var a = document.getElementById("email")
+// var newTxt = document.createTextNode("manoa0741@gmail.com");
+// a.appendChild(newTxt);
+
+//Task2
+
+//i.
+
+// var a = document.getElementById("form-content");
+// console.log(a.nodeType)
+
+//ii.
+// var a = document.getElementById("last-name");
+// console.log(a.nodeType)
+// console.log(a.childNodes)
+
+//iii.
+// var a = document.getElementById("last-name");
+// var b = document.createElement("img")
+// console.log(b)
+
+//iv.
+// var a = document.getElementById("main-content");
+// console.log(a.firstChild)
+// console.log(a.lastChild)
+
+//v.
+// var a = document.getElementById("last-name");
+// console.log(a.nextSibling)
+// console.log(a.previousSibling)
+
+//vi.
+// var a = document.getElementById("email");
+// console.log(a.nodeType)
+// console.log(a.parentNode)
